832b0dd994ac6fe3c1477da2dac56e4d5b060783
Deivison7777 <143571154+Deivison7777@users.noreply.github.com>
2023-11-08 16:42:35 -0300

    Atividade 14.c

---
+++ Atividade 14.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int numero;
+    int somapares = 0;
+    int somaimpares = 0;
+    int numpares = 0;
+    int numimpares = 0;
+    float mediaimpares;
+    float mediapares;
+
+    printf("Digite números inteiros (digite um valor negativo para encerrar):\n");
+
+    while (1) {
+        printf("Digite um número: ");
+        scanf("%d", &numero);
+
+        if (numero < 0) {
+            break;
+        }
+
+        if (numero % 2 == 0) {
+            somapares += numero;
+            numpares++;
+        } else {
+            somaimpares += numero;
+            numimpares++;
+        }
+    }
+
+    if (numpares > 0) {
+        float mediapares = somapares / numpares;
+        printf("Números pares: %d (Média: %.2f)\n", numpares, mediapares);
+    } else {
+        printf("Nenhum número par foi digitado.\n");
+    }
+
+    if (numimpares > 0) {
+        float mediaimpares = somaimpares / numimpares;
+        printf("Números ímpares: %d (Média: %.2f)\n", numimpares, mediaimpares);
+    } else {
+        printf("Nenhum número ímpar foi digitado.\n");
+    }
+
+    return 0;
+}
